6552922403a821e38d57cec580415d0348ca4ed2
Jada-kay Williams <jakaywill@gmail.com>
2024-12-01 23:19:43 -0500

    Added initial starter files

---
+++ world.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const phpfile = 'http://localhost/info2180-lab5/world.php';
+    const srchBttn = document.getElementById('search');
+    const ctysrchBttn = document.getElementById('citysearch');
+    const srchBar = document.getElementById('country');
+    const result = document.getElementById('result');
+
+    const getRequest = (query, option) => {
+        fetch(`${phpfile}?country=${encodeURIComponent(query)}&lookup=${encodeURIComponent(option)}`)
+            .then(response => response.text())
+            .then(data => {
+                result.innerHTML = data;
+            })
+            .catch(error => {
+                alert(`Error: ${error}`);
+            });
+    };
+
+    ctysrchBttn.addEventListener('click', () => {
+        const query = srchBar.value.trim();
+        getRequest(query, 'cities');
+    });
+
+    srchBttn.addEventListener('click', () => {
+        const query = srchBar.value.trim();
+        getRequest(query, '');
+    });
+});
